f16aef5ed5a85c229b7676c25bf7ff104b4576b8
Richard <notzree@gmail.com>
2023-02-20 15:55:59 -0500

    removed github signin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 
 
@@ -7,10 +6,6 @@ export const authOptions = {
     // Remember to change the GCP URI once deployed
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     GoogleProvider({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET,
