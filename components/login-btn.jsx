4ea4e08e5cd9398d4ed1843467a95420f176ef85
Richard Zhang <notzree@Richards-MacBook-Air.local>
2023-02-21 00:16:05 -0500

    formatting wit prettier

--- components/login-btn.jsx
+++ components/login-btn.jsx
@@ -1,44 +1,59 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+/* eslint-disable @next/next/no-img-element */
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function LoginButton() {
-  const { data: session } = useSession()
-  
+  const { data: session } = useSession();
+
   if (session) {
     return (
       <>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src = {session? session.user.image: "https://img.favpng.com/20/11/12/computer-icons-user-profile-png-favpng-0UAKKCpRRsMj5NaiELzw1pV7L.jpg"}/>
+        <div className="dropdown-end dropdown">
+          <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
+            <div className="w-10 rounded-full">
+              <img
+              alt = "Profile Photo"
+                src={
+                  session
+                    ? session.user.image
+                    : "https://img.favpng.com/20/11/12/computer-icons-user-profile-png-favpng-0UAKKCpRRsMj5NaiELzw1pV7L.jpg"
+                }
+              />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a className="justify-between">{session.user.name}</a>
+            </li>
+            <li>
+              <button onClick={() => signOut()} className="btn-primary btn">
+                Sign out
+              </button>
+            </li>
+          </ul>
         </div>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a className="justify-between">
-          {session.user.name}
-          </a></li>
-        <li><button onClick={() => signOut()} className = "btn btn-primary">Sign out</button></li>
-      </ul>
-    </div>
       </>
-    )
+    );
   }
   return (
     <>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <button className="w-10 rounded-full" onClick={()=>signIn()}>
-          <img src = "https://img.favpng.com/20/11/12/computer-icons-user-profile-png-favpng-0UAKKCpRRsMj5NaiELzw1pV7L.jpg"/>
-          </button>
-        </div>
-      </label>
-    </div>
+      <div className="dropdown-end dropdown">
+        <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
+          <div className="w-10 rounded-full">
+            <button className="w-10 rounded-full" onClick={() => signIn()}>
+              <img src="https://img.favpng.com/20/11/12/computer-icons-user-profile-png-favpng-0UAKKCpRRsMj5NaiELzw1pV7L.jpg" />
+            </button>
+          </div>
+        </label>
+      </div>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import LoginButton from '@/components/login-btn'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import LoginButton from "@/components/login-btn";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,9 +15,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="">
-      <div className='flex justify-center items-center'>
-      </div>
+        <div className="flex items-center justify-center"></div>
       </main>
     </>
-  )
+  );
 }
