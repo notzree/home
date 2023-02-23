40d0d5e6eea317b9eb7dbf281a6473cb3f84b967
Richard Zhang <notzree@Richards-MacBook-Air.local>
2023-02-22 22:13:49 -0500

    Can create listing using form

---
+++ pages/api/listing/createListing.js
@@ -0,0 +1,32 @@
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../auth/[...nextauth]"
+import client from "@/lib/prismadb"
+export default async function handler(req, res) {
+    const session = await getServerSession(req, res, authOptions)
+    if (session) {
+        const body = JSON.parse(req.body);
+        // console.log(body);
+        const userId = await prisma.user.findUnique({
+            where: {
+              email: body.email
+            },
+          })
+        console.log(body.data.listingName);
+          var data = {
+            // owner: body.data.owner,
+            owner: userId.id,
+            listingLocationCoords: "Empty for now",
+            listingCategory: "Rabbit",
+           listingName:body.data.listingName,
+           listingSex: body.data.listingSex,
+           listingAge: body.data.listingAge,
+           listingPrice: body.data.listingPrice,
+           listingDescription: body.data.listingDescription
+          }
+        const createdListing = await client.listings.create({data:data})
+
+    } else {
+        res.status(400).json({ message: "Please Sign in"})
+    }
+  }
+  
--- pages/createListingPage.js
+++ pages/createListingPage.js
@@ -1,11 +1,53 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+
+
 
 export default function Home() {
+    const { data: session } = useSession();
+    const router = useRouter();
+    console.log(session.user)
+    const  handleSubmit = async (e)=>{
+        if(!session){
+            router.push("/createListingPage");
+        }
+        e.preventDefault();
+        var listingsex = "";
+        if(e.target.listingGenderF.checked){
+            listingsex = "Female"
+        } else{
+            listingsex = "Male"
+        }
+       var userEmail = session.user.email;
+
+        var body = {
+            data:{
+                listingLocationCoords: "Empty for now",
+                listingCategory: "Rabbit",
+               listingName:e.target.listingName.value,
+               listingSex: listingsex,
+               listingAge: parseInt(e.target.listingAge.value) ,
+               listingPrice: parseInt(e.target.listingPrice.value),
+               listingDescription: e.target.listingDescription.value
+            },
+
+            email:userEmail
+        }
+        const response = await fetch("/api/listing/createListing",{
+            method: "POST",
+            body:JSON.stringify(body)
+        })
+        if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+    }
   return (
     <>
       <main className="h-screen w-screen bg-primary">
-        <div className=" flex h-screen w-screen items-center justify-center">
-          <form className="form-control w-full max-w-xs ">
+        <div className=" mt-10 flex h-screen w-screen items-center justify-center">
+          <form className="form-control w-full max-w-xs "
+          onSubmit={handleSubmit}
+          >
             <label htmlFor="text" className="label">
               <span className=" label-text text-secondary">Pet Name</span>
             </label>
@@ -15,7 +57,15 @@ export default function Home() {
               placeholder="Type here"
               class="input-bordered input my-2 w-full max-w-xs"
             />
-
+            <label htmlFor="text" className="label">
+              <span className=" label-text text-secondary">Pet Age</span>
+            </label>
+            <input
+              id="listingAge"
+              type="text"
+              placeholder="Type here"
+              class="input my-2 w-full max-w-xs"
+            />
             <label htmlFor="text" className="label">
               <span className=" label-text text-secondary">
                 Rehoming Fee (leave as 0 for free / no cost)
@@ -29,7 +79,8 @@ export default function Home() {
             />
 
             <div className="flex flex-row items-center justify-evenly">
-              <label htmlFor="text" className="label">
+                
+                <label htmlFor="text" className="label">
                 <span className=" label-text text-secondary">Sex:</span>
               </label>
 
@@ -41,7 +92,7 @@ export default function Home() {
                 type="radio"
                 name="radio-10"
                 className="radio-secondary radio checked:bg-red-500"
-                checked
+                checked 
               />
               <label className="label">
                 <span className="label-text text-secondary">Male</span>
@@ -51,15 +102,19 @@ export default function Home() {
                 type="radio"
                 name="radio-10"
                 className="radio-secondary radio checked:bg-blue-500"
-                checked
+                checked 
               />
             </div>
             <div>
               <label className="label" htmlFor="text">
                 <span className="label-text text-secondary">Animal Category</span>
               </label>
-              <select class="select-secondary select w-full max-w-xs">
-                <option disabled selected>
+              <select id="listingCategory" class="select-secondary select w-full max-w-xs"
+              defaultValue={"DEFAULT"}
+              >
+                <option disabled 
+                value = "DEFAULT"
+                >
                   Pick your Category
                 </option>
                 <option>Dogs</option>
@@ -70,6 +125,12 @@ export default function Home() {
                 <option>Rodents / Related</option>
                 <option>Other</option>
               </select>
+            </div>
+            <div className=" w-full flex flex-col justify-center items-center py-2">
+                <label className="label" htmlFor="text">
+                <span className="label-text text-secondary">Description</span>
+              </label>
+            <textarea id="listingDescription" className="textarea textarea-bordered textarea-lg w-full h-40" placeholder="Description"></textarea>
             </div>
             <button className="btn-secondary btn my-2">Submit</button>
           </form>
